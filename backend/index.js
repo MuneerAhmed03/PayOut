a46cc90ee9b86cceca6f0ff70d7896864442d08d
MuneerAhmed03 <muneerahmed00916@gmail.com>
2024-03-24 23:51:51 +0530

    Port enviornment variable

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ const rootRouter= require('./routes/index')
 const cors=require("cors")
 
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json())
 app.use("/api/v1",rootRouter);
 
-app.listen(3000);
+app.listen(PORT);
 
