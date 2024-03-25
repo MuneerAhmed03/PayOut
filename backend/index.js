e2d0dc65295746b8ce579de0ccbdd2d49650edde
MuneerAhmed03 <muneerahmed00916@gmail.com>
2024-03-25 10:47:24 +0530

    cors origin

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors=require("cors")
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-    origin: '*',
+    origin: 'https://pay-out.vercel.app/',
     optionsSuccessStatus: 200 
 }
 
