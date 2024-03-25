d49a4ae721ffc138524578d633b0a7439426d349
MuneerAhmed03 <muneerahmed00916@gmail.com>
2024-03-25 06:33:05 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors=require("cors")
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
